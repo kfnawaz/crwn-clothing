22ef4cb6a7b585a46a72bc9752d7564c0debcdf1
Nawazuddin MohammedKhaja <kfnawaz@gmail.com>
2020-04-04 11:51:35 -0500

    update out app with a shop page, collectin preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from './pages/homepage/homepage.component';
-
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1> HATS PAGE </h1>
-  </div>
-);
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/hats' component={HatsPage} />
-          <HomePage />
-        </Switch>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className='App'>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
